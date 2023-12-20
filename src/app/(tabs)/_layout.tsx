09b4e6e1ed6f95fb27f9b6fc6a1869ca979521ad
Alan-Graton <graton.alan@gmail.com>
2023-12-20 19:04:46 -0300

    wip: start Home header development

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,13 @@
 import { Tabs } from "expo-router";
 
+import { View, Text } from "react-native";
+import { Image } from "native-base";
+
 import {
   House,
   UserCircle,
   ClockCounterClockwise,
+  SignOut,
 } from "phosphor-react-native";
 
 export default function TabsLayout() {
@@ -11,18 +15,98 @@ export default function TabsLayout() {
     <Tabs initialRouteName="(home)">
       <Tabs.Screen
         name="(home)"
-        options={{ title: "Home", tabBarIcon: () => <House /> }}
+        options={{
+          title: "",
+          header: () => (
+            <>
+              <View
+                style={{
+                  flexDirection: "row",
+                  backgroundColor: "#202024",
+                  padding: 6,
+                  height: 125,
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Image
+                  source={{ uri: "https://github.com/Alan-Graton.png" }}
+                  width={50}
+                  height={50}
+                  ml={2}
+                  rounded="full"
+                  borderWidth={2}
+                  borderColor="gray.400"
+                  alt="Profile Picture"
+                />
+                <View style={{ flex: 1, padding: 6, marginLeft: 12 }}>
+                  <Text style={{ color: "white", fontSize: 14 }}>Olá,</Text>
+                  <Text
+                    style={{ color: "white", fontSize: 16, fontWeight: "bold" }}
+                  >
+                    Alan Graton
+                  </Text>
+                </View>
+                <View style={{ marginRight: 10 }}>
+                  <SignOut color="white" />
+                </View>
+              </View>
+            </>
+          ),
+          tabBarIcon: () => <House />,
+        }}
       />
       <Tabs.Screen
         name="(history)"
         options={{
-          title: "History",
+          title: "",
+          header: () => (
+            <>
+              <View
+                style={{
+                  backgroundColor: "#202024",
+                  padding: 4,
+                  height: 125,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, color: "#E1E1E6", fontWeight: "bold" }}
+                >
+                  Histórico de Exercícios
+                </Text>
+              </View>
+            </>
+          ),
           tabBarIcon: () => <ClockCounterClockwise />,
         }}
       />
       <Tabs.Screen
         name="(profile)"
-        options={{ title: "Profile", tabBarIcon: () => <UserCircle /> }}
+        options={{
+          title: "",
+          header: () => (
+            <>
+              <View
+                style={{
+                  backgroundColor: "#202024",
+                  padding: 4,
+                  height: 125,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, color: "#E1E1E6", fontWeight: "bold" }}
+                >
+                  Perfil
+                </Text>
+              </View>
+            </>
+          ),
+          tabBarIcon: () => <UserCircle />,
+        }}
       />
     </Tabs>
   );
