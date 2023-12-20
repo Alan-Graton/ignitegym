3708e1de94a6b8c9fa50ba4d1a91e3aa5fb34dc5
Alan-Graton <graton.alan@gmail.com>
2023-12-20 12:13:36 -0300

    wip: login screen is done

--- src/app/(login)/index.tsx
+++ src/app/(login)/index.tsx
@@ -1,11 +1,52 @@
-import { Text } from "react-native";
+import { Center, Text, Heading, VStack, Image, ScrollView } from "native-base";
 
-import { SafeAreaView } from "react-native-safe-area-context";
+import { AppTextInput } from "@/components/AppTextInput";
+import { AppButton } from "@/components/AppButton";
+
+import LogoSvg from "@/assets/logo.svg";
+import BackgroundImg from "@/assets/background.png";
 
 export default function Login() {
   return (
-    <SafeAreaView>
-      <Text>Oi Alan</Text>
-    </SafeAreaView>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10}>
+        <Image
+          source={BackgroundImg}
+          alt="Background Image"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm" fontFamily="body">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Acesse sua conta
+          </Heading>
+          <AppTextInput
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <AppTextInput placeholder="Senha" secureTextEntry />
+
+          <AppButton title="Acessar" />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" mb={3} fontSize="sm" fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+          <AppButton title="Criar conta" variant="outline" />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 }
