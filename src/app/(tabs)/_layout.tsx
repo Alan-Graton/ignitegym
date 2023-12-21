de21394f834de5837e67040fdd513f41bc648b78
Alan-Graton <graton.alan@gmail.com>
2023-12-20 23:29:38 -0300

    wip: setting icons active/inactive colors and weigth on focus/blur

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import { THEME } from "@/theme";
 import { Tabs } from "expo-router";
 
 import {
@@ -8,20 +9,51 @@ import {
 
 export default function TabsLayout() {
   return (
-    <Tabs initialRouteName="(home)">
+    <Tabs
+      initialRouteName="(home)"
+      screenOptions={{
+        tabBarStyle: { height: 75, backgroundColor: THEME.colors.gray[600] },
+        tabBarActiveTintColor: THEME.colors.green[500],
+        tabBarInactiveTintColor: THEME.colors.gray[300],
+      }}
+    >
       <Tabs.Screen
         name="(home)"
-        options={{ title: "", tabBarIcon: () => <House /> }}
+        options={{
+          title: "",
+          tabBarIcon: ({ size, color }) => (
+            <House
+              size={size}
+              color={color}
+              weight={color === THEME.colors.green[500] ? "bold" : "regular"}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="(history)"
-        options={{ title: "", tabBarIcon: () => <ClockCounterClockwise /> }}
+        options={{
+          title: "",
+          tabBarIcon: ({ size, color }) => (
+            <ClockCounterClockwise
+              size={size}
+              color={color}
+              weight={color === THEME.colors.green[500] ? "bold" : "regular"}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name="(profile)"
         options={{
           title: "",
-          tabBarIcon: () => <UserCircle />,
+          tabBarIcon: ({ size, color }) => (
+            <UserCircle
+              size={size}
+              color={color}
+              weight={color === THEME.colors.green[500] ? "bold" : "regular"}
+            />
+          ),
         }}
       />
     </Tabs>
