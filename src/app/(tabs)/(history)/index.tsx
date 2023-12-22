af853f545ffb12b0b5aca4351bb28f11e9badaa6
Alan-Graton <graton.alan@gmail.com>
2023-12-21 22:03:13 -0300

    wip: finishing History screen

--- src/app/(tabs)/(history)/index.tsx
+++ src/app/(tabs)/(history)/index.tsx
@@ -1,14 +1,40 @@
 import React from "react";
 
-import { Box, Center, VStack, HStack, Text } from "native-base";
+import { VStack, Heading, SectionList, Text } from "native-base";
+
+import { HistoryCard } from "./components/HistoryCard";
 
 export default function HistoryProfile() {
+  const [exercises, setExercises] = React.useState<
+    { title: string; data: any[] }[]
+  >([
+    { title: "22.08.22", data: ["Puxada Frontal", "Remada unilateral"] },
+    { title: "27.08.22", data: ["Puxada frontal"] },
+  ]);
+
   return (
     <>
-      <VStack flex={1} bg="gray.700" padding={6}>
-        <Center>
-          <Text color="white">History Screen Slot</Text>
-        </Center>
+      <VStack flex={1} bg="gray.700">
+        <SectionList
+          sections={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <HistoryCard />}
+          renderSectionHeader={({ section }) => (
+            <Heading color="gray.200" fontSize="md" mt={10} m={3}>
+              {section.title}
+            </Heading>
+          )}
+          px={8}
+          contentContainerStyle={
+            exercises.length === 0 && { flex: 1, justifyContent: "center" }
+          }
+          ListEmptyComponent={() => (
+            <Text color="gray.100" textAlign="center">
+              Não há exercícios registrados ainda. {"\n"}
+              Vamos fazer exercícios hoje?
+            </Text>
+          )}
+        />
       </VStack>
     </>
   );
