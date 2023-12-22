2aac98706380d56bce8868afa7bcd3154dfc6083
Alan-Graton <graton.alan@gmail.com>
2023-12-21 22:11:28 -0300

    wip: remove unused imports

--- src/app/(tabs)/(profile)/_layout.tsx
+++ src/app/(tabs)/(profile)/_layout.tsx
@@ -1,8 +1,6 @@
 import { AppSimpleScreenHeader } from "@/components/AppSimpleScreenHeader";
 import { Tabs, Slot } from "expo-router";
 
-import { Box, Center, Text } from "native-base";
-
 export default function ProfileLayout() {
   return (
     <>
