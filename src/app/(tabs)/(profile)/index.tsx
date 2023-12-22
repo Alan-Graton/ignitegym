90eaca05c6464fd315e6d2e7aa6064301257a432
Alan-Graton <graton.alan@gmail.com>
2023-12-21 22:29:54 -0300

    wip: finishing Profile screen

--- src/app/(tabs)/(profile)/index.tsx
+++ src/app/(tabs)/(profile)/index.tsx
@@ -1,15 +1,70 @@
 import React from "react";
 
-import { Box, Center, VStack, HStack, Text } from "native-base";
+import { TouchableOpacity } from "react-native";
+import {
+  Center,
+  VStack,
+  Heading,
+  Text,
+  ScrollView,
+  Skeleton,
+} from "native-base";
+
+import { AppUserPicture } from "@/components/AppUserPicture";
+import { AppTextInput } from "@/components/AppTextInput";
+import { AppButton } from "@/components/AppButton";
+
+const PIC_SIZE: number = 33;
 
 export default function Profile() {
   return (
-    <>
-      <VStack flex={1} bg="gray.700" padding={6}>
-        <Center>
-          <Text color="white">Profile Screen Slot</Text>
+    <VStack flex={1} bg="gray.700">
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {/* <Skeleton
+            w={PIC_SIZE}
+            h={PIC_SIZE}
+            rounded="full"
+            startColor="gray.500"
+            endColor="gray.400"
+          /> */}
+          <AppUserPicture size={PIC_SIZE} />
+          <TouchableOpacity>
+            <Text color="green.500" fontSize="md" mt={2} mb={8} bold>
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <AppTextInput placeholder="Nome" bg="gray.600" />
+          <AppTextInput
+            value="graton.alan@gmail.com"
+            placeholder="E-mail"
+            isDisabled
+            bg="gray.600"
+          />
         </Center>
-      </VStack>
-    </>
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+          <AppTextInput
+            placeholder="Senha antiga"
+            secureTextEntry
+            bg="gray.600"
+          />
+          <AppTextInput
+            placeholder="Nova senha"
+            secureTextEntry
+            bg="gray.600"
+          />
+          <AppTextInput
+            placeholder="Confirmar nova senha"
+            secureTextEntry
+            bg="gray.600"
+          />
+          <AppButton title="Atualizar" mt={4} />
+        </VStack>
+      </ScrollView>
+    </VStack>
   );
 }
