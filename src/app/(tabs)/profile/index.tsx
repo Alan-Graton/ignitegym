32a1b20696eb5245e6b40d70c35c264739b685f5
Alan-Graton <graton.alan@gmail.com>
2023-12-23 09:54:49 -0300

    feat: changing user pic

--- src/app/(tabs)/profile/index.tsx
+++ src/app/(tabs)/profile/index.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { UserContext } from "@/contexts/UserContext";
+
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { TouchableOpacity } from "react-native";
 import {
@@ -14,22 +18,71 @@ import { AppUserPicture } from "@/components/AppUserPicture";
 import { AppTextInput } from "@/components/AppTextInput";
 import { AppButton } from "@/components/AppButton";
 
+import { STATIC_USER_PICTURE } from "@/constants";
 const PIC_SIZE: number = 33;
 
 export default function Profile() {
+  const { user, setUser } = React.useContext(UserContext);
+
+  const [isPicLoading, setIsPicLoading] = React.useState<boolean>(false);
+
+  // TODO: Apply D.R.Y
+  const handleUserPicture = user.picture ? user.picture : STATIC_USER_PICTURE;
+
+  async function handleChangePicture() {
+    try {
+      setIsPicLoading(true);
+
+      const response = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 4],
+        quality: 1,
+      });
+
+      if (response.canceled) return;
+
+      if (response.assets[0].uri) {
+        const picInfo = await FileSystem.getInfoAsync(response.assets[0].uri);
+
+        setUser((prevState) => ({
+          ...prevState,
+          picture: response.assets[0].uri,
+        }));
+      }
+    } catch (e) {
+      console.error(
+        "\n\n[Profile] Error while selecting new Profile Picture: ",
+        e
+      );
+
+      return;
+    } finally {
+      setIsPicLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1} bg="gray.700">
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
-          {/* <Skeleton
-            w={PIC_SIZE}
-            h={PIC_SIZE}
-            rounded="full"
-            startColor="gray.500"
-            endColor="gray.400"
-          /> */}
-          <AppUserPicture size={PIC_SIZE} />
-          <TouchableOpacity>
+          {isPicLoading && (
+            <Skeleton
+              w={PIC_SIZE}
+              h={PIC_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          )}
+          {!isPicLoading && (
+            <AppUserPicture
+              source={{ uri: handleUserPicture }}
+              size={PIC_SIZE}
+            />
+          )}
+
+          <TouchableOpacity onPress={handleChangePicture}>
             <Text color="green.500" fontSize="md" mt={2} mb={8} bold>
               Alterar foto
             </Text>
