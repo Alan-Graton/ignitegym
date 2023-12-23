c74865c6d8d5e449b61a0790f7f7206410a94566
Alan-Graton <graton.alan@gmail.com>
2023-12-23 00:02:59 -0300

    wip-fix: ui adjusts in `ExerciseDetails` Screen Header

--- src/app/(tabs)/home/exercise_details/_layout.tsx
+++ src/app/(tabs)/home/exercise_details/_layout.tsx
@@ -1,7 +1,7 @@
 import { router, Stack } from "expo-router";
 
 import { TouchableOpacity } from "react-native";
-import { Heading, HStack, Icon, Text, VStack } from "native-base";
+import { Box, Heading, HStack, Icon, Text, VStack } from "native-base";
 
 import { Feather } from "@expo/vector-icons";
 import BodySvg from "@/assets/body.svg";
@@ -17,35 +17,30 @@ export default function ExerciseDetailsLayout() {
       screenOptions={{
         headerShown: true,
         header: () => (
-          <VStack pt={5} pb={5} height={117} bg="gray.600" safeArea>
-            <VStack bg="gray.600" px={8}>
-              <TouchableOpacity onPress={handleGoBack}>
-                <Icon
-                  as={Feather}
-                  name="arrow-left"
-                  color="green.500"
-                  size={6}
-                />
-              </TouchableOpacity>
+          <VStack bg="gray.600" px={8} pt={5} safeArea>
+            <TouchableOpacity>
+              <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+            </TouchableOpacity>
 
-              <HStack
-                justifyContent="space-between"
-                my={4}
-                mb={8}
-                alignItems="center"
-              >
+            <HStack
+              justifyContent="space-between"
+              mt={3.5}
+              mb={3.5}
+              alignItems="center"
+            >
+              <HStack>
                 <Heading color="gray.100" fontSize="lg" flexShrink={1}>
                   Puxada frontal
                 </Heading>
-
-                <HStack alignItems="center">
-                  <BodySvg />
-                  <Text color="gray.200" ml={1} textTransform="capitalize">
-                    Costas
-                  </Text>
-                </HStack>
               </HStack>
-            </VStack>
+
+              <HStack alignItems="center">
+                <BodySvg />
+                <Text color="gray.200" ml={1} textTransform="capitalize">
+                  Costas
+                </Text>
+              </HStack>
+            </HStack>
           </VStack>
         ),
       }}
