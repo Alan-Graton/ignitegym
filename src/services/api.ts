e711e897ee0297abd77a8c062223210c471f315d
Alan-Graton <graton.alan@gmail.com>
2023-12-25 15:48:34 -0300

    wip: create new `AppError` util and new Axios instance

---
+++ src/services/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+import { AppError } from "@/utils/AppError";
+
+console.log("\n\nAPI Base URL: ", process.env.EXPO_PUBLIC_IP_ADDRESS);
+
+const api = axios.create({
+  baseURL: `http://${process.env.EXPO_PUBLIC_IP_ADDRESS}:${process.env.EXPO_PUBLIC_PORT}/`,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(
+        new AppError("Erro no servidor. Tente de novo mais tarde.")
+      );
+    }
+  }
+);
+
+export { api };
