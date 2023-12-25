be93eb50d5443e670f4031b49e9e03d4b06985f3
Alan-Graton <graton.alan@gmail.com>
2023-12-25 17:49:01 -0300

    wip: create `DEFAULT_VALUE` to `UserDTO`

--- src/dtos/UserDTO.ts
+++ src/dtos/UserDTO.ts
@@ -1,6 +1,15 @@
-export interface UserDTO {
+interface UserDTO {
   id: string;
   name: string;
   email: string;
   picture: string;
 }
+
+const DEFAULT_VALUE = {
+  id: "",
+  name: "",
+  email: "",
+  picture: "",
+};
+
+export { UserDTO, DEFAULT_VALUE };
