07850e303f99c2f3b7b90435277d9c2af0b55eb2
Alan-Graton <graton.alan@gmail.com>
2023-12-26 11:21:05 -0300

    wip: create `storageAuthTokenRemove` and minor refac in error messages

--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -6,16 +6,32 @@ export async function storageAuthTokenSave(token: string): Promise<void> {
   try {
     await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token);
   } catch (error) {
-    console.error("\n\n[Storage Auth Token Save] Error: ", error);
+    console.error(
+      "\n\n[storageAuthToken] storageAuthTokenSave FAILED: ",
+      error
+    );
   }
 }
 
-export async function storageAuthTokenGet(): Promise<string | null | undefined> {
+export async function storageAuthTokenGet(): Promise<
+  string | null | undefined
+> {
   try {
     const token = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE);
 
     return token;
   } catch (error) {
-    console.error("\n\n[Storage Auth Token Save] Error: ", error);
+    console.error("\n\n[storageAuthToken] storageAuthTokenGet FAILED: ", error);
+  }
+}
+
+export async function storageAuthTokenRemove(): Promise<void> {
+  try {
+    await AsyncStorage.removeItem(AUTH_TOKEN_STORAGE);
+  } catch (error) {
+    console.error(
+      "\n\n[storageAuthToken] storageAuthTokenRemove FAILED: ",
+      error
+    );
   }
 }
