bf9c6074ea022dca5e12fa73bf8d48285d76d2db
Alan-Graton <graton.alan@gmail.com>
2023-12-26 12:07:25 -0300

    feat: create new `ExerciseDTO`

---
+++ src/dtos/ExercisesDTO.ts
@@ -0,0 +1,10 @@
+export interface ExerciseDTO {
+  id: string; // string?
+  demo: string;
+  group: string;
+  name: string;
+  repetitions: number;
+  series: number;
+  thumb: string;
+  updated_at: string;
+}
