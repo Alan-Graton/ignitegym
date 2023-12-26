6645bba140832320cf910c6e257ee01d025e4917
Alan-Graton <graton.alan@gmail.com>
2023-12-26 08:57:41 -0300

    wip: user is redirected to Home when still being authenticated

--- src/app/(login)/_layout.tsx
+++ src/app/(login)/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+
+import { useAuthContext } from "@/hooks/useAuthContext";
 
 export default function LoginLayout() {
+  const { user } = useAuthContext();
+
+  if (user.id) return <Redirect href="/home/" />;
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
